59a28f8006b102bd57797ef55029c97b3bfd1ad9
Furkan Ulutaş <furkanulutas@icloud.com>
2024-05-18 21:57:23 +0300

    chore: blog kart dizaynı yapıldı ve sayfalar arası routing sağlandı

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { Header } from "./components/Header";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,37 @@
-﻿export default function Home() {
+﻿import { animated, useSpring } from "@react-spring/web";
+import PostCard from "../components/blog-items/PostCard";
+import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
+
+export default function Home() {
+  const springs = useSpring({
+    from: { opacity: 0, transform: "translateY(-100px)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    delay: 200,
+  });
   return (
     <>
-      <div className="flex justify-center ">
-        <div className=" flex gap-2 p-32 text-6xl font-bold text-white ">
+      <animated.div className="flex justify-center" style={springs}>
+        <div className=" flex gap-2 px-32 py-16 text-6xl font-bold text-slate-700 dark:text-white ">
           Welcome to the{" "}
           <div className="bg-gradient-to-r from-indigo-700 to-blue-500 px-5 text-center">
-            <span className="text-white"> Our Blog</span>
+            <span className="text-white dark:text-black"> Our Blog</span>
           </div>
         </div>
+      </animated.div>
+      <div className="m-12 flex flex-wrap justify-center gap-5">
+        <PostCard blogId="123123" />
+        <PostCard />
+        <PostCard />
+      </div>
+      <div className="flex justify-center">
+        <div className="h-0.5 w-1/2 bg-gray-300 dark:bg-gray-300"></div>
+      </div>
+      F
+      <div className="mt-5 flex justify-center">
+        <Link to={"/blog"}>
+          <Button gradientDuoTone="greenToBlue">Discover More</Button>
+        </Link>
       </div>
     </>
   );
