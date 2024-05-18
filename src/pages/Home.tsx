70d98c7dabeac9d8addec5a93f1047a0ea88376e
Furkan Ulutaş <furkanulutas@icloud.com>
2024-05-18 18:07:46 +0300

    created base

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+﻿export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center ">
+        <div className=" flex gap-2 p-32 text-6xl font-bold text-white ">
+          Welcome to the{" "}
+          <div className="bg-gradient-to-r from-indigo-700 to-blue-500 px-5 text-center">
+            <span className="text-white"> Our Blog</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
